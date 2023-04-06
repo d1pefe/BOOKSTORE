57db350ec9ef900d33a80084e7eeb5e0ea30702a
d1pefe <113125565+d1pefe@users.noreply.github.com>
2023-04-06 19:40:09 +0300

    Refactoring, global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
 import React from "react";
 
-import styles from "./App.module.scss";
-
 import Button from "./components/Button";
-import {ButtonTypes} from "./components/Button/Button";
+import { ButtonTypes } from "./components/Button/Button";
+import Input from "./components/Input";
 
 function App() {
     return (
-        <div className={styles.App}>
-            <Button
-                title={"Main"}
-                types={ButtonTypes.Main}
-                onClick={() => {}}
-            />
-            <Button
-                title={"Closing"}
-                types={ButtonTypes.Closing}
-                onClick={() => {}}
-            />
-            <Button
-                title={"Arrow"}
-                onClick={() => {}}
-                types={ButtonTypes.Arrow}
-            />
-            <Button
-                title={"Arrow"}
-                onClick={() => {}}
-                types={ButtonTypes.Like}
-            />
+        <div>
+            {/*<Button*/}
+            {/*    title={"Main"}*/}
+            {/*    types={ButtonTypes.Main}*/}
+            {/*    onClick={() => {}}*/}
+            {/*/>*/}
+            {/*<Button*/}
+            {/*    title={"Closing"}*/}
+            {/*    types={ButtonTypes.Closing}*/}
+            {/*    onClick={() => {}}*/}
+            {/*/>*/}
+            {/*<Button*/}
+            {/*    title={"Arrow"}*/}
+            {/*    onClick={() => {}}*/}
+            {/*    types={ButtonTypes.Arrow}*/}
+            {/*/>*/}
+            {/*<Button*/}
+            {/*    title={"Like"}*/}
+            {/*    onClick={() => {}}*/}
+            {/*    types={ButtonTypes.Like}*/}
+            {/*/>*/}
+            <Input inputType={"text"} title={"Text"} onChange={()=>{}} placeholder={"Your text"} />
         </div>
     );
 }
