903713211b676aae1bb8b27b1d7a00f43c6e79b0
d1pefe <113125565+d1pefe@users.noreply.github.com>
2023-04-08 19:32:14 +0300

    Tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,47 @@
 import React from "react";
 
 import Button from "./components/Button";
-import { ButtonTypes } from "./components/Button/Button";
+import {ButtonTypes} from "./components/Button/Button";
 import Input from "./components/Input";
+import Tabs from "./components/Tabs";
 
 function App() {
     return (
         <div>
-            {/*<Button*/}
-            {/*    title={"Main"}*/}
-            {/*    types={ButtonTypes.Main}*/}
-            {/*    onClick={() => {}}*/}
-            {/*/>*/}
-            {/*<Button*/}
-            {/*    title={"Closing"}*/}
-            {/*    types={ButtonTypes.Closing}*/}
-            {/*    onClick={() => {}}*/}
-            {/*/>*/}
-            {/*<Button*/}
-            {/*    title={"Arrow"}*/}
-            {/*    onClick={() => {}}*/}
-            {/*    types={ButtonTypes.Arrow}*/}
-            {/*/>*/}
-            {/*<Button*/}
-            {/*    title={"Like"}*/}
-            {/*    onClick={() => {}}*/}
-            {/*    types={ButtonTypes.Like}*/}
-            {/*/>*/}
-            <Input inputType={"text"} title={"Text"} onChange={()=>{}} placeholder={"Your text"} />
+            <Button
+                title={"Main"}
+                types={ButtonTypes.Main}
+                onClick={() => {}}
+            />
+            <Button
+                title={"Closing"}
+                types={ButtonTypes.Closing}
+                onClick={() => {}}
+            />
+            <Button
+                title={"Arrow"}
+                onClick={() => {}}
+                types={ButtonTypes.Arrow}
+            />
+            <Button
+                title={"Like"}
+                onClick={() => {}}
+                types={ButtonTypes.Like}
+            />
+            <Input
+                inputType={"text"}
+                title={"Text"}
+                onChange={() => {}}
+                placeholder={"Your text"}
+            />
+            <Input
+                inputType={"text"}
+                title={"Text2"}
+                onChange={() => {}}
+                placeholder={"Your text2"}
+                disabled
+            />
+            <Tabs />
         </div>
     );
 }
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,13 +1,60 @@
-import React, { FC } from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
 
 import styles from "./Tabs.module.scss";
 
+export enum TabsNames {
+  DESCRIPTION,
+  AUTHORS,
+  REVIEWS,
+}
+
+const TABS_LIST = [
+  {
+    key: TabsNames.DESCRIPTION,
+    title: "Description",
+    disabled: false,
+  },
+  {
+    key: TabsNames.AUTHORS,
+    title: "Authors",
+    disabled: false,
+  },
+  {
+    key: TabsNames.REVIEWS,
+    title: "Reviews",
+    disabled: true,
+  },
+];
 
 const Tabs = () => {
+  const [activeTab, setActiveTab] = useState(TabsNames.DESCRIPTION);
+
+  const onTabClick = (key: TabsNames) => {
+    return () => setActiveTab(key);
+  };
 
   return (
-    <div></div>
+    <div className={styles.container}>
+      {TABS_LIST.map((tab) => {
+        return (
+          <div
+            key={tab.key}
+            className={classNames(styles.tab, {
+              [styles.activeTab]: activeTab === tab.key,
+              [styles.disabledTab]: tab.disabled,
+            })}
+            onClick={
+              tab.disabled || activeTab === tab.key
+                ? undefined
+                : onTabClick(tab.key)
+            }
+          >
+            {tab.title}
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
